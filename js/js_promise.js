255d4ea42c6e0edc414b3ed714dc463d95310c01
jsinsun <1274903955@qq.com>
2021-12-07 15:48:24 +0800

    数据扁平化

--- js/js_promise.js
+++ js/js_promise.js
@@ -1,5 +1,5 @@
 //Promise
-var promise = new Promise((resolve, reject) => {
+const promise = new Promise((resolve, reject) => {
     resolve('fulfilled');
 })
 
@@ -16,13 +16,27 @@ const promise3 = new Promise((resolve, reject) => {
     reject('rejected3');
 })
 
-Promise.all([apromise, promise1, promise2, promise3]).then(res => {
+Promise.all([promise, promise1, promise2, promise3]).then(res => {
     console.log(res)
 }).catch(error => {
     console.log(error)
 })
 
 
-promise.then(res => promise1.then(res1 => {
-    console.log(res1)
-}));
+
+new Promise((resolve, reject) => {
+    resolve(1)
+}).then(value => {
+    console.log('onResolved1', value)
+    return new Promise((resolve, reject) => {
+        reject('新的promise');
+    })
+}, reason => {
+    console.log('onRejected1', reason)
+
+}).then(value => { //状态由上一次的回调结果决定
+    console.log('onResolved2', value)
+}, reason => {
+    // return undefined
+    console.log('onRejected2', reason)
+})
